122a359f345ed7b1a7e078fa805069498c344f05
Andika Dafan <dikadafan@gmail.com>
2025-06-16 05:14:35 +0700

    Fix: match export name for Data

--- src/app/Halaman/page/5-ternak-uang/25/container/data.ts
+++ src/app/Halaman/page/5-ternak-uang/25/container/data.ts
@@ -1,9 +1,37 @@
 export const Data = [
   {
     id: "1",
-    title: "Big Crypto Thesis",
-    drive: "https://drive.google.com/file/d/1h4whupHDhFwlsvCpjHfzw0rUdRNmKpjt",
-    link: "/Halaman/page/5-ternak-uang/1-Crypto-Investing/1-Big-Crypto-Thesis",
-    category: "Crypto Investing",
+    title: "Konsep Dasar Pajak",
+    drive: "https://drive.google.com/file/d/1B49cAOIT7Pgd_J6096BGaQDCqZNIGFO9",
+    link: "/Halaman/page/5-ternak-uang/25/1",
+    category: "Stock and Finance",
+  },
+  {
+    id: "2",
+    title: "Istilah Umum Pajak",
+    drive: "https://drive.google.com/file/d/1LFiPJIWE3DzrS5ppb6Ka7ZPHNVwKXFJC",
+    link: "/Halaman/page/5-ternak-uang/25/2",
+    category: "Stock and Finance",
+  },
+  {
+    id: "3",
+    title: "Konsep Dasar Pajak Penghasilan",
+    drive: "https://drive.google.com/file/d/1XSHXfq804-snbJdGU71-hW8rORbBmjGJ",
+    link: "/Halaman/page/5-ternak-uang/25/3",
+    category: "Stock and Finance",
+  },
+  {
+    id: "4",
+    title: "Mengenal Pajak Penghasilan Pada Investasi Pasar Uang",
+    drive: "https://drive.google.com/file/d/1qkA5AHgZogT6fEmbNjArYa55Dgo6HPYJ",
+    link: "/Halaman/page/5-ternak-uang/25/4",
+    category: "Stock and Finance",
+  },
+  {
+    id: "5",
+    title: "Mengenal Pajak Penghasilan Pada Investasi Aset Digital & Fintech",
+    drive: "https://drive.google.com/file/d/1alGjK8laj_94U617RR4SPNFtYQXUWov8",
+    link: "/Halaman/page/5-ternak-uang/25/5",
+    category: "Stock and Finance",
   },
 ];
