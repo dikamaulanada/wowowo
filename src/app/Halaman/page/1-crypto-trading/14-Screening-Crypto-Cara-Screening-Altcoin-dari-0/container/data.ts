71d3be3925fcd1f89cd6c31dff47c45bac9aee2f
Andika Dafan <dikadafan@gmail.com>
2025-06-16 16:25:00 +0700

    Fix: match export name for Data

--- src/app/Halaman/page/1-crypto-trading/14-Screening-Crypto-Cara-Screening-Altcoin-dari-0/container/data.ts
+++ src/app/Halaman/page/1-crypto-trading/14-Screening-Crypto-Cara-Screening-Altcoin-dari-0/container/data.ts
@@ -5,5 +5,5 @@ export const Data = [
       "drive": "https:/drive.google.com/file/d/1WP2gXgykxOuhZD1bUUdEaG3ukLpk4T7G/view?usp=drive_link",
       "link": "/Halaman/page/1-crypto-trading/14-Screening-Crypto-Cara-Screening-Altcoin-dari-0/01-Cara-Screening-Altcoin-dari-0",
       "category": "Crypto Trading"
-    },
-],
+    }
+]
